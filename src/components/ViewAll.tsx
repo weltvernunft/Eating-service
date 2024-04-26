ff9d161362750c64e141444da76a76a2ecc3508e
Blackdolpfinlove <balckdolphinlove@gmail.com>
2024-04-26 14:10:20 +0700

    svg-hovering

--- src/components/ViewAll.tsx
+++ src/components/ViewAll.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const ViewAll: React.FC = () => {
   return (
-    <div className="view-all w-full relative mt-4">
-      <button className="absolute md:right-0 text-gray-400 | hover:text-global-shade transition-colors">View all ➔</button>
+    <div className="view-all w-full relative mt-8">
+      <button className="absolute md:right-0 text-gray-400 | hover:text-global-shade transition-colors">
+        View all ➔
+      </button>
     </div>
   );
 };
